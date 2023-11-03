9c0b61ef06a50246eff628d64b43ed1a2ed81431
TheComputerM <mudit.somani00@gmail.com>
2023-11-03 15:47:56 +0530

    feat: generate random signatures

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use jwt_cracker::size;
+use random_string::generate;
 
 fn main() {
     let token = std::env::args().nth(1).expect("Provide Token");
 
     let len = size(token);
+    let charset = "1234567890abcdefghijklmnopqrtuvwxzy.";
 
-    println!("{}", len);
+    let test_signature = generate(len, charset);
+
+    println!("{}", test_signature);
 }
