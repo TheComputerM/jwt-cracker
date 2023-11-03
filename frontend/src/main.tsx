1392bc43e034ea63b390ec601d51480bb2708b57
TheComputerM <mudit.somani00@gmail.com>
2023-11-03 15:53:40 +0530

    chore: cleanup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,5 @@ import '@builder.io/qwik/qwikloader.js'
 
 import { render } from '@builder.io/qwik'
 import { App } from './app.tsx'
-import './index.css'
 
 render(document.getElementById('app') as HTMLElement, <App />)
