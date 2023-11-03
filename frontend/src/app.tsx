d873f4ce71b40030dc7c01a32a4e0eebc3708148
TheComputerM <mudit.somani00@gmail.com>
2023-11-03 15:59:27 +0530

    feat: add ui

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,4 +1,12 @@
-import { component$, useSignal, $ } from '@builder.io/qwik';
+import { component$, useSignal, $, Slot } from '@builder.io/qwik';
+ 
+const Button = component$(() => {
+  return (
+    <button>
+      Content: <Slot />
+    </button>
+  );
+});
 
 export const App = component$(() => {
   const count = useSignal(0);
@@ -7,6 +15,10 @@ export const App = component$(() => {
     <>
       <button onClick$={increment}>Increment</button>
       <p>Count: {count.value}</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime deleniti culpa quam repellat consectetur molestiae minima vel odit quis veniam quae dolor rerum sapiente omnis, accusamus natus in iusto provident magnam at ab explicabo enim blanditiis fugiat? Nobis ipsa eligendi dolorum, numquam eos quis a ipsam repellendus repellat. Possimus neque placeat similique veritatis reprehenderit odio id accusamus repellat ea facere voluptates sequi provident rerum natus vel, commodi dolores amet.
+      </p>
+      <Button>I will kill god</Button>
     </>
   );
 });
